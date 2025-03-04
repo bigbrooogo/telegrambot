e2ad8387ecbee8549233db1d16785492dd3bf3c9
bigbrooogo <serko107@gmail.com>
2025-03-04 16:30:01 +0300

    JRTB 0: added spring boot skeleton

---
+++ src/test/java/com/bigbrooogo/github/javarushtelegrambot/JavarushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbrooogo.github.javarushtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
